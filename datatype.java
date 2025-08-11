3ca71a8c53c79eb89dd5203dd6635be30b9d1fdb
Priyachauhan_1192 <priyachauhan51574@gmail.com>
2025-08-11 23:59:08 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,17 @@
+package JAVA;
+
+public class datatype {
+    public static void main(String[] args){
+        int age = 20;
+        float pi = 3.14f;
+        double d = 22.56;
+        char grade = 'A';
+        boolean isPass = true;
+        short s = 3200;
+        long l = 12345678L;
+        System.out.println(age);
+        System.out.println(pi);
+        System.out.println(d);
+        System.out.println(grade);
+    }
+}
---
+++ variable.java
@@ -0,0 +1,25 @@
+package JAVA;
+
+public class variable {
+    public static void main(String[] args){
+        String myNameIs = "Priya";
+        int marksInExam = 856;
+        int myAge = 20;
+        int numberInPhysics = 76;
+        int a = 5; //local variable
+        int _b = 7; 
+        int $c = 8;
+        System.out.println(myNameIs);
+        System.out.println(myAge);
+        System.out.println(numberInPhysics);
+        System.out.println(marksInExam);
+        System.out.println(a);
+        System.out.println(_b);
+        System.out.println($c);
+        int sum = a+_b;
+        System.out.println(sum);
+        System.out.println(a+_b);
+    }
+}
+
+

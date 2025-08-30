b9152c95aedc88b87dbf1efb59d91d173ea53af7
Priya Chauhan <priyachauhan51574@gmail.com>
2025-08-30 15:25:26 +0530

    Sum of digits in a number

---
+++ ASSIGNMENT/Assignment1/q5.java
@@ -0,0 +1,19 @@
+package ASSIGNMENT.Assignment1;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        while(n > 0){
+            int r = n % 10;
+            sum = sum + r;
+            n = n/10;
+        }
+        System.out.println(sum);
+    }
+    
+}

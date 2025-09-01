01101e17dd71c189152f52ea42e234d81076679f
justtpriya-12 <priyachauhan51574@gmail.com>
2025-09-01 11:26:17 +0530

    right alphabet triangle

---
+++ hPatternProblems/rightAlphaTri.java
@@ -0,0 +1,18 @@
+package hPatternProblems;
+
+import java.util.Scanner;
+
+public class rightAlphaTri {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print((char)(j+96)+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}

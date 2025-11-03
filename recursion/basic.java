fa0c3cc59c6d9a4e34bc47e37ad1bc28f80b384f
justtpriya-12 <priyachauhan51574@gmail.com>
2025-11-03 22:14:31 +0530

    stack-overflow

---
+++ recursion/basic.java
@@ -0,0 +1,11 @@
+//stack overflow
+public class basic {
+    public static void myName(){
+        System.out.println("Hello");
+        myName();
+    }
+    public static void main(String[] args) {
+        myName();
+    }
+    
+}
